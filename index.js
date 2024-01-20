b4318298ebb7ad0e2629cf607c0a30b60eb75980
nijgururajofficial <nijgururaja12@gmail.com>
2024-01-20 11:28:19 +0530

    authorization

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const { connectToMongoDB } = require("./connect.js");
-const { restrictToUserLoginOnly, checkAuth } = require("./middlewares/auth.js");
+const { checkForAuthentication, restrictTo } = require("./middlewares/auth.js");
 
 const URL = require("./models/url");
 const app = express();
@@ -21,17 +21,11 @@ app.set("views", path.resolve("./views"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(checkForAuthentication);
 
-app.get("/test", async (req, res) => {
-  const allUrls = await URL.find({});
-  return res.render("home", {
-    urls: allUrls,
-  });
-});
-
-app.use("/url", restrictToUserLoginOnly, urlRoute);
+app.use("/url", restrictTo(["NORMAL"]), urlRoute);
 app.use("/user", userRoute);
-app.use("/", checkAuth, staticRoute);
+app.use("/", staticRoute);
 
 app.get("/url/:shortid", async (req, res) => {
   const shortId = req.params.shortid;
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,24 +1,29 @@
 const { getUser } = require("../service/auth");
 
-async function restrictToUserLoginOnly(req, res, next) {
-  const userUid = req.cookies?.uid;
-  if (!userUid) return res.redirect("/login");
-  const user = getUser(userUid);
+function checkForAuthentication(req, res, next) {
+  const tokenCookie = req.cookies?.token;
+  req.user = null;
 
-  if (!user) return res.redirect("/login");
+  if (!tokenCookie) return next();
+
+  const token = tokenCookie;
+  const user = getUser(token);
+
   req.user = user;
-  next();
+  return next();
 }
 
-async function checkAuth(req, res, next) {
-  const userUid = req.cookies?.uid;
-  const user = getUser(userUid);
+function restrictTo(roles = []) {
+  return function (req, res, next) {
+    if (!req.user) return res.redirect("/login");
 
-  req.user = user;
-  next();
+    if (!roles.includes(req.user.role)) return res.end("UnAuthorized");
+
+    return next();
+  };
 }
 
 module.exports = {
-  restrictToUserLoginOnly,
-  checkAuth,
+  checkForAuthentication,
+  restrictTo,
 };
